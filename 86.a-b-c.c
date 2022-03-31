666d2b39e55e016b5cc0b9439dbb1fa78536fb93
bakenray <lpqstrong@163.com>
2022-03-31 10:49:02 +0800

    add: a*b*c

--- 86.a-b-c.c
+++ 86.a-b-c.c
@@ -6,10 +6,14 @@
  ************************************************************************/
 
 #include<stdio.h>
+
 int main(){
+
     int a,b,c;
+
     while(scanf("%d%d%d",&a,&b,&c) != EOF){  
         printf("%d\n",a*b*c);
-    }    
+    }
+
     return 0;
 }
