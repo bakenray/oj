04a2bfa92e988658a1cdea50eb49802e57e477c1
bakenray <lpqstrong@163.com>
2022-03-31 10:47:44 +0800

    add: output n even

--- 39.n-even.c
+++ 39.n-even.c
@@ -5,15 +5,20 @@
 	> Created Time: Thu 31 Mar 2022 09:45:00 AM CST
  ************************************************************************/
 
-#include<stdio.h>
+#include<stdio.h
+>
 int main(){
+
     int begin,n;
     scanf("%d%d",&begin,&n);
+
     if(begin<0) begin = 0;
     if(begin%2) begin += 1;
+
     for(int i=0; i<n;i++){
         printf("%d\n",begin);
         begin+=2;
     }
+
     return 0;
 }
