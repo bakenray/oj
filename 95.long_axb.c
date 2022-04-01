8bc71e77c66cddf22618cb6b88fdaee3ce20a32c
bakenray <lpqstrong@163.com>
2022-04-01 11:48:41 +0800

    feat: axb

---
+++ 95.long_axb.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: 95.long_axb.c
+	> Author: 
+	> Mail: 
+	> Created Time: Fri 01 Apr 2022 11:42:41 AM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main(){
+    long long a, b;
+    scanf("%lld%lld",&a, &b);
+    printf("%lld\n", a * b);
+}
