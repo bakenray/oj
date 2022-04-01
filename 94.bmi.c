e87820f3e2ac43cb9735a424d7bc6f02e29f306f
bakenray <lpqstrong@163.com>
2022-04-01 11:41:27 +0800

    add: bmi

---
+++ 94.bmi.c
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: 94.bmi.c
+	> Author: 
+	> Mail: 
+	> Created Time: Fri 01 Apr 2022 11:38:57 AM CST
+ ************************************************************************/
+
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    double w,h;
+    scanf("%lf%lf",&w,&h);
+    printf("%.2lf\n",w/pow(h,2));
+    return 0;
+}
